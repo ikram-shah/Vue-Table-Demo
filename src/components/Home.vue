7f47d21de2fda8936ee582dfc029b1d7aec153ad
Ikram Shah <ikram2shah@gmail.com>
2019-05-04 03:30:20 +0530

    Responsiveness Added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,27 +8,27 @@
 
       <b-container class="text-center" fluid>
         <div class="d-flex justify-content-center row">
-          <b-card title="Official Website" tag="article" class="col-3 pad">
+          <b-card title="Official Website" tag="article" class="col-lg-3 col-sm-6 pad">
             <b-card-text>Visit the Official website of Vuetable-2 Datatable Simplified!</b-card-text>
             <b-button href="https://www.vuetable.com/" variant="primary">GO</b-button>
           </b-card>
 
-          <b-card title="Official Git Repo" tag="article" class="col-3 pad">
+          <b-card title="Official Git Repo" tag="article" class="col-lg-3 col-sm-6 pad">
             <b-card-text>Visit the Official Github Repository of Vuetable-2</b-card-text>
             <b-button href="https://www.vuetable.com/" variant="primary">GO</b-button>
           </b-card>
 
-          <b-card title="Vuetable-2 Tutorial" tag="article" class="col-3 pad">
+          <b-card title="Vuetable-2 Tutorial" tag="article" class="col-lg-3 col-sm-6 pad">
             <b-card-text>This is seriosly awesome if you starting with Vue!</b-card-text>
             <b-button href="https://github.com/ratiw/vuetable-2-tutorial/wiki" variant="primary">GO</b-button>
           </b-card>
 
-          <b-card title="Codepen of Creator" tag="article" class="col-3 pad">
+          <b-card title="Codepen of Creator" tag="article" class="col-lg-3 col-sm-6 pad">
             <b-card-text>Get more Vuetable-2 reference projects/ pens here!</b-card-text>
             <b-button href="https://codepen.io/ratiw/" variant="primary">GO</b-button>
           </b-card>
 
-          <b-card title="Repo of this website" tag="article" class="col-3 pad">
+          <b-card title="Repo of this website" tag="article" class="col-lg-3 col-sm-6 pad">
             <b-card-text>This demo website's Github Repository!</b-card-text>
             <b-button href="https://github.com/ikram-shah/vue-table-demo" variant="primary">GO</b-button>
           </b-card>
@@ -36,7 +36,7 @@
           <b-card
             title="Credits to the Creator Ratiw & the open source community"
             tag="article"
-            class="col-3 pad"
+            class="col-lg-3 col-sm-6 pad"
           >
             <h1>❤️</h1>
           </b-card>
