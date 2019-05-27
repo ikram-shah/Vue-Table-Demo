0f2ebe821a6c52835556adf665365f8b1c7d489c
Ikram Shah <ikram2shah@gmail.com>
2019-05-27 20:14:35 +0530

    PostConf

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,11 +13,6 @@
             <b-button href="https://www.vuetable.com/" variant="primary">GO</b-button>
           </b-card>
 
-          <b-card title="Official Git Repo" tag="article" class="col-lg-3 col-sm-6 pad">
-            <b-card-text>Visit the Official Github Repository of Vuetable-2</b-card-text>
-            <b-button href="https://www.vuetable.com/" variant="primary">GO</b-button>
-          </b-card>
-
           <b-card title="Vuetable-2 Tutorial" tag="article" class="col-lg-3 col-sm-6 pad">
             <b-card-text>This is seriosly awesome if you starting with Vue!</b-card-text>
             <b-button href="https://github.com/ratiw/vuetable-2-tutorial/wiki" variant="primary">GO</b-button>
@@ -27,6 +22,12 @@
             <b-card-text>Get more Vuetable-2 reference projects/ pens here!</b-card-text>
             <b-button href="https://codepen.io/ratiw/" variant="primary">GO</b-button>
           </b-card>
+
+           <b-card title="Slide Deck" tag="article" class="col-lg-3 col-sm-6 pad">
+            <b-card-text>Visit the Slide Deck Presented in the VueConf.</b-card-text>
+            <b-button href="https://drive.google.com/file/d/1LtwggCo1_Cyg1zXB1LLbif_nfpXroDbN/view?usp=sharing" download="VueTable" variant="primary">GO</b-button>
+          </b-card>
+
 
           <b-card title="Repo of this website" tag="article" class="col-lg-3 col-sm-6 pad">
             <b-card-text>This demo website's Github Repository!</b-card-text>
@@ -42,9 +43,36 @@
           </b-card>
         </div>
       </b-container>
+
+      <hr>
+
+ <h2>Post Conference</h2>
+
+<p> Thank you <a class="twt" href="https://twitter.com/znck0">@Rahul</a> & <a class="twt" href="https://twitter.com/SwapAgarwal">@Swapnil</a> for organizing this wonderful conference. It was a great experience and learning </p>
+
+
+      <b-container class="row text-center" fluid >
+        <Tweet class="col-4" id="1124623360787238913"></Tweet>
+          <Tweet class="col-4" id="1124624771969196032"></Tweet>
+         <Tweet class="col-4" id="1124650843800621056"></Tweet>
+        
+    
+      </b-container>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+import { Tweet, Moment, Timeline } from 'vue-tweet-embed'
+
+export default {
+  components: {
+    Tweet
+  },
+  
+}
+</script>
+
 
 <style>
 .card-body {
@@ -53,6 +81,10 @@
 
 .pad {
   margin: 10px;
+}
+
+.twt{
+  color: white;
 }
 </style>
 
